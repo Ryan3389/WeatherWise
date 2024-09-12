d24ca3983fda2c1bcbf0e576752950ea93a80514
Ryan3389 <Ryan.Cuthbert12@gmail.com>
2024-09-12 12:13:25 -0400

    added routes for pages

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,36 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { HeroPage } from './pages/HomePage.jsx'
+import { loginPage } from './pages/LoginPage.jsx'
+import { SignupPage } from './pages/SignupPage.jsx'
+import { ErrorPage } from './pages/ErrorPage.jsx';
+
 import App from './App.jsx'
 import './index.css'
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        element: <HeroPage />
+      },
+      {
+        path: '/login',
+        element: <loginPage />
+      },
+      {
+        path: '/signup',
+        element: <SignupPage />
+      }
+    ]
+  }
+])
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+  <RouterProvider router={router} />
+);
---
+++ client/src/pages/LoginPage.jsx
@@ -0,0 +1,5 @@
+export const loginPage = () => {
+    return (
+        <h1>Login Page</h1>
+    )
+}
