f6d697ef1ec080110116d366031ea674b718ec01
Ryan3389 <Ryan.Cuthbert12@gmail.com>
2024-09-14 11:25:06 -0400

    added front end auth for login

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,31 @@
+import { gql } from '@apollo/client'
+
+export const CREATE_USER = `
+    mutation Mutation($firstName: String!, $lastName: String!, $email: String!, $password: String!) {
+  createUser(firstName: $firstName, lastName: $lastName, email: $email, password: $password) {
+    token
+    user {
+      _id
+      firstName
+      lastName
+      email
+      password
+    }
+  }
+}
+`
+
+export const LOGIN_USER = gql`
+    mutation Mutation($email: String!, $password: String!) {
+  login(email: $email, password: $password) {
+    token
+    user {
+      _id
+      firstName
+      lastName
+      email
+      password
+    }
+  }
+}
+`
